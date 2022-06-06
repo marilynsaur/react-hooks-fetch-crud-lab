8fc14a868dfd0cac1e9bb41e395eb220254b7df3
Marilyn Wren <Marilynwren64@yahoo.com>
2022-06-06 11:30:32 -0400

    Done.

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function QuestionList() {
+  //const [selectedCategory, setSelectedCategory] = useState("All");
+  const [items, setItems] = useState([]);
+
+  
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
